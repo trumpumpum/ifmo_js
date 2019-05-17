375700ca14f32ccd992bf1cc672ee3f6155a7e16
trumpumpum <gapess@ya.ru>
2019-05-18 00:49:47 +0300

    Добавил parseInt

--- 1/hw1_3.js
+++ 1/hw1_3.js
@@ -1,9 +1,17 @@
 //Третья задача
 //Из трех данных чисел выбрать наименьшее.
 
-let first = prompt("Введите 1 число");
-let second = prompt("Введите 2 число");
-let third = prompt("Введите 3 число");
-let compare_1 = (first <= second) ? compare_1 = first: compare_1 = second;
-compare_1 = (compare_1 <= third)? compare_1 = compare_1: compare_1 = third;
-alert(compare_1);
+let first = parseInt(prompt("Введите 1 число"));
+let second = parseInt(prompt("Введите 2 число"));
+let third = parseInt(prompt("Введите 3 число"));
+
+
+
+let compare_1;
+compare_1 = (first <= second) ? compare_1 = first : compare_1 = second;
+console.log(compare_1);
+let compare_2;
+compare_2 = (compare_1 <= third) ? compare_2 = compare_1 : compare_2 = third;
+alert(compare_2);
+
+
