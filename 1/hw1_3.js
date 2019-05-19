2685931cf1fbe43a4771ed82110649f9e0664a77
trumpumpum <gapess@ya.ru>
2019-05-19 16:49:43 +0300

    Поправил замечания

--- 1/hw1_3.js
+++ 1/hw1_3.js
@@ -7,11 +7,6 @@ let third = parseInt(prompt("Введите 3 число"));
 
 
 
-let compare_1;
-compare_1 = (first <= second) ? compare_1 = first : compare_1 = second;
-console.log(compare_1);
-let compare_2;
-compare_2 = (compare_1 <= third) ? compare_2 = compare_1 : compare_2 = third;
+let compare_1 = (first <= second) ? first :  second;
+let compare_2 = (compare_1 <= third) ? compare_1 : third;
 alert(compare_2);
-
-
--- 1/hw1_4.js
+++ 1/hw1_4.js
@@ -2,10 +2,10 @@
 //4. Вывести в консоль ближайшее к 10 из двух чисел, записанных в переменные m и n. 
 //Например, среди чисел 8.5 и 11.45 ближайшее к десяти 11.45.
 
-let m = prompt ("Введите первое число");
-let n = prompt ("Введите второе число");
+let m = parseFloat(prompt ("Введите первое число"));
+let n = parseFloat(prompt ("Введите второе число"));
 m = m - 10;
 n = n - 10;
-let compare = (m <= n) ? console.log(m+10) : console.log(n+10);
+let compare = (Math.abs(m) <= Math.abs(n)) ? console.log(m + 10) : console.log(n + 10);
 
 
